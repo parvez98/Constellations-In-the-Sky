d30639a2de80dd8e3065afaa76ce9f7b9a05dd27
Soha Parvez <31773939+parvez98@users.noreply.github.com>
2018-06-25 02:02:30 +0530

    Add files via upload

---
+++ myjavascript.js
@@ -0,0 +1,96 @@
+		var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d");
+		ctx.beginPath();
+
+		//draco
+		ctx.moveTo(295, 345);
+		ctx.lineTo(325, 315);
+	
+		ctx.moveTo(325, 315);
+		ctx.lineTo(375, 175);
+		
+		ctx.moveTo(375, 175);
+		ctx.lineTo(375, 145);
+	
+		ctx.moveTo(375, 145);
+		ctx.lineTo(410, 175);
+	
+		ctx.moveTo(410, 175);
+		ctx.lineTo(420, 195);
+
+		ctx.moveTo(420, 195);
+		ctx.lineTo(395, 270);
+		
+		ctx.moveTo(395, 270);
+		ctx.lineTo(405, 325);
+		
+		ctx.moveTo(405, 325);
+		ctx.lineTo(395, 340);
+		
+		ctx.moveTo(395, 340);
+		ctx.lineTo(415, 365);
+		
+		ctx.moveTo(415, 365);
+		ctx.lineTo(465, 375);
+
+		ctx.moveTo(465, 375);
+		ctx.lineTo(525, 395);
+
+		ctx.moveTo(525, 395);
+		ctx.lineTo(545, 415);
+		
+		ctx.moveTo(295, 345);
+		ctx.lineTo(320, 360);
+
+		ctx.moveTo(320, 360);
+		ctx.lineTo(335, 345);
+
+		ctx.moveTo(335, 345);
+		ctx.lineTo(325, 315);
+
+		//canis-major
+		ctx.moveTo(695, 415);
+		ctx.lineTo(725, 365);
+		
+		ctx.moveTo(725, 365);
+		ctx.lineTo(755, 345);
+		
+		ctx.moveTo(755, 345);
+		ctx.lineTo(765, 295);
+		
+		ctx.moveTo(765, 295);
+		ctx.lineTo(805, 195);
+		
+		ctx.moveTo(805, 195);
+		ctx.lineTo(775, 205);
+		
+		ctx.moveTo(775, 205);
+		ctx.lineTo(745, 185);
+		
+		ctx.moveTo(745, 185);
+		ctx.lineTo(765, 145);
+		
+		ctx.moveTo(765, 145);
+		ctx.lineTo(805, 195);
+		
+		ctx.moveTo(805, 195);
+		ctx.lineTo(845, 235);
+		
+		ctx.moveTo(845, 235);
+		ctx.lineTo(885, 210);
+
+		ctx.moveTo(845, 235);
+		ctx.lineTo(875, 265);
+
+		ctx.moveTo(845, 235);
+		ctx.lineTo(805, 295);
+
+		ctx.moveTo(805, 295);
+		ctx.lineTo(815, 385);
+
+		ctx.moveTo(815, 385);
+		ctx.lineTo(755, 345);
+
+		ctx.strokeStyle = 'white';
+		ctx.lineWidth = 2.5;
+		ctx.stroke();
